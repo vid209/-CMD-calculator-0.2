f50b57567123a67019e378898f8c9ea3fc3104b7
Макс Max <biserovm3@gmail.com>
2025-08-19 10:48:21 +0300

    Add files via upload

---
+++ cmd can/cmd can/cmd can.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <limits>
+
+int main() {
+    double num1, num2, res;
+    char math;
+
+    std::cout << "enter num1 : ";
+    while (!(std::cin >> num1)) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input. Enter a number: ";
+    }
+
+    std::cout << "enter num2 : ";
+    while (!(std::cin >> num2)) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input. Enter a number: ";
+    }
+
+    std::cout << "enter math symbol(+, -, *, / ) : ";
+    while (!(std::cin >> math) || (math != '+' && math != '-' && math != '*' && math != '/')) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid operator. Enter +, -, *, or /: ";
+    }
+
+    switch (math) {
+    case '+':
+        res = num1 + num2;
+        break;
+    case '-':
+        res = num1 - num2;
+        break;
+    case '*':
+        res = num1 * num2;
+        break;
+    case '/':
+        if (num2 == 0.0) {
+            std::cout << "Error: Division by zero! " << std::endl;
+            return 1;
+        }
+        res = num1 / num2;
+        break;
+    default:
+        std::cout << "Error: Invalid operator!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Result: " << res << std::endl;
+    return 0;
+}
